017da500a5deae3aef93917f22b51e8d56b406a1
Carlos Santillan <carlos@carloss-macbook-pro.local>
2018-10-09 16:48:09 -0400

    Data Persistent using User Defaults

--- todoey/TodoListViewController.swift
+++ todoey/TodoListViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Find Mike", "Buy Weed", "Destory his Inventory"]
+    var itemArray : Array<String> = Array()
+    let defaults = UserDefaults.standard
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
 
     }
 
@@ -24,7 +30,7 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = String(itemArray[indexPath.row])
         return cell
     }
     
@@ -47,7 +53,10 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default)/*Closure ->*/ {(action) in
           self.itemArray.append(textField.text!)
-          self.tableView.reloadData()
+        
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+            self.tableView.reloadData()
             
         }
         alert.addTextField { (alertTextField) in
