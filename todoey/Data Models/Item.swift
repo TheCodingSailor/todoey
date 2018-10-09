c849cb844775841365c06070c7f79b5b58f4a43b
Carlos Santillan <carlos@carloss-macbook-pro.local>
2018-10-09 17:55:44 -0400

    convert to MVC

---
+++ todoey/Data Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  todoey
+//
+//  Created by Carlos on 10/9/18.
+//  Copyright © 2018 Carlos Santillan. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
